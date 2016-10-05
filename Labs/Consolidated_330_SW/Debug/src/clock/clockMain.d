b9bf9814ad183164ab36fc8b3457ba9e29f434f4
Connor Moody <Connor Moody>
2016-10-05 17:26:08 -0600

    lab 4 rewrite

--- Labs/Consolidated_330_SW/Debug/src/clock/clockMain.d
+++ Labs/Consolidated_330_SW/Debug/src/clock/clockMain.d
@@ -1,21 +1,20 @@
 src/clock/clockMain.o src/clock/clockMain.d: ../src/clock/clockMain.c \
- ../../Consolidated_330_SW_bsp/ps7_cortexa9_0/include/xparameters.h \
- ../../Consolidated_330_SW_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
  C:\Users\cdmoo\Desktop\BYU_FALL_2016\ECEN_330\XilinxWorkspace\Labs\Consolidated_330_SW/supportFiles/leds.h \
  C:\Users\cdmoo\Desktop\BYU_FALL_2016\ECEN_330\XilinxWorkspace\Labs\Consolidated_330_SW/supportFiles/globalTimer.h \
+ ../../Consolidated_330_SW_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../Consolidated_330_SW_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
  ../../Consolidated_330_SW_bsp/ps7_cortexa9_0/include/xil_types.h \
  C:\Users\cdmoo\Desktop\BYU_FALL_2016\ECEN_330\XilinxWorkspace\Labs\Consolidated_330_SW/supportFiles/interrupts.h \
  ../src/clock/clockControl.h ../src/clock/clockDisplay.h \
- ../src/clock/../intervalTimer/intervalTimer.h \
  C:\Users\cdmoo\Desktop\BYU_FALL_2016\ECEN_330\XilinxWorkspace\Labs\Consolidated_330_SW/supportFiles/display.h
-
-../../Consolidated_330_SW_bsp/ps7_cortexa9_0/include/xparameters.h:
-
-../../Consolidated_330_SW_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
 
 C:\Users\cdmoo\Desktop\BYU_FALL_2016\ECEN_330\XilinxWorkspace\Labs\Consolidated_330_SW/supportFiles/leds.h:
 
 C:\Users\cdmoo\Desktop\BYU_FALL_2016\ECEN_330\XilinxWorkspace\Labs\Consolidated_330_SW/supportFiles/globalTimer.h:
+
+../../Consolidated_330_SW_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../Consolidated_330_SW_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
 
 ../../Consolidated_330_SW_bsp/ps7_cortexa9_0/include/xil_types.h:
 
@@ -24,7 +23,5 @@ C:\Users\cdmoo\Desktop\BYU_FALL_2016\ECEN_330\XilinxWorkspace\Labs\Consolidated_
 ../src/clock/clockControl.h:
 
 ../src/clock/clockDisplay.h:
-
-../src/clock/../intervalTimer/intervalTimer.h:
 
 C:\Users\cdmoo\Desktop\BYU_FALL_2016\ECEN_330\XilinxWorkspace\Labs\Consolidated_330_SW/supportFiles/display.h:
